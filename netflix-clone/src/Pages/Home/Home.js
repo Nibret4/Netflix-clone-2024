1646e3832ece8c3277c7594fc42b1eae14ded970
Nibert <nibertaddis@gmail.com>
2024-04-27 09:03:59 -0400

    headr and foter

---
+++ netflix-clone/src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../componets/heaer/Header";
+import Footer from "../../componets/footer/Footer";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
